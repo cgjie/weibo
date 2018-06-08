d989d68936a98ce0febda62afe18599613aa6e85
ChenGuangJie <chengj@healthmall.cn>
2018-06-08 11:20:30 +0800

    初始化项目

---
+++ weibo/weibo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  weibo
+//
+//  Created by chengungjie on 2018/6/8.
+//  Copyright © 2018年 jkm_cgj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
